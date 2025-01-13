c12c43bc11dafdf93b5027c2c042ea60bd8eff6e
coderunner02 <premansuchakraborty02@gmail.com>
2025-01-13 22:52:03 +0530

    Third version

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [jokes, setjokes] = useState([])
 
    useEffect(()=>{
-    axios.get('/jokes')
+    axios.get('http://localhost:3000/jokes')
         .then(function (response) {
           console.log(response.data);
           setjokes(response.data)
